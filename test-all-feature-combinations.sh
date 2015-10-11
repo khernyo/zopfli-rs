9a9edb9e3462e50e8b0f0bd8e9d7ff1ac97a7a41
Szabolcs Berecz <szabolcs.berecz@gmail.com>
2015-10-11 16:15:51 +0200

    Add script for running tests with all feature combinations

---
+++ test-all-feature-combinations.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+export RUST_BACKTRACE=1
+
+ALL_FEATURES=(longest-match-cache hash-same hash-same-hash shortcut-long-repetitions lazy-matching)
+
+(( LIMIT = (1 << ${#ALL_FEATURES[@]}) - 1))
+for i in `seq 0 $LIMIT`; do
+    unset FEATURES
+    declare -a FEATURES
+    for f in `seq 0 $(expr ${#ALL_FEATURES[@]} - 1)`; do
+        (( USE = ($i & (1 << $f)) )) || true
+        if [ $USE -gt 0 ]; then
+            FEATURES+=(${ALL_FEATURES[$f]})
+        fi
+    done
+    echo "Using features: ${FEATURES[@]}"
+    echo "=============="
+    cargo test --verbose --no-default-features --features "`echo ${FEATURES[@]}`"
+done
