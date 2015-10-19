5f7b8cfd684265439fe32c1e0ae14ac311278ca4
Szabolcs Berecz <szabolcs.berecz@gmail.com>
2015-10-20 00:43:08 +0200

    Add zlib_container.rs

--- src/zlib_container.rs
+++ src/zlib_container.rs
@@ -1,12 +1,63 @@
-use std::ptr;
+//! Functions to compress according to the Zlib specification.
 
-use libc::{c_void, size_t};
-use libc::funcs::c95::stdlib::{free, malloc};
+use std::io::Write;
 
 use super::Options;
 
+use deflate::deflate;
+
+/// Calculates the adler32 checksum of the data
+unsafe fn adler32(mut data: *const u8, mut size: usize) -> u32 {
+    const SUMS_OVERFLOW: u32 = 5550;
+    let mut s1: u32 = 1;
+    let mut s2: u32 = 1 >> 16;
+
+    while size > 0 {
+        let mut amount: usize = if size > SUMS_OVERFLOW as usize { SUMS_OVERFLOW as usize } else { size };
+        size -= amount;
+        while amount > 0 {
+            s1 += *data as u32;
+            data = data.offset(1);
+            s2 += s1;
+            amount -= 1;
+        }
+        s1 %= 65521;
+        s2 %= 65521;
+    }
+
+    (s2 << 16) | s1
+}
+
+/**
+ * Compresses according to the zlib specification and append the compressed
+ * result to the output.
+ *
+ * options: global program options
+ * out: pointer to the dynamic output array to which the result is appended. Must
+ *   be freed after use.
+ * outsize: pointer to the dynamic output array size.
+ */
 pub unsafe fn compress(options: *const Options, input: *const u8, insize: usize, out: *mut *mut u8, outsize: *mut usize) {
-    *out = malloc(insize as u64) as *mut u8;
-    ptr::copy_nonoverlapping(input, *out, insize);
-    *outsize = insize;
+    let mut bitpointer: u8 = 0;
+    let checksum: u32 = adler32(input, insize as u32 as usize);
+    let cmf: u32 = 120; // CM 8, CINFO 7. See zlib spec.
+    let flevel: u32 = 0;
+    let fdict: u32 = 0;
+    let mut cmfflg: u32 = 256 * cmf + fdict * 32 + flevel * 64;
+    let fcheck: u32 = 31 - cmfflg % 31;
+    cmfflg += fcheck;
+
+    append_data!((cmfflg / 256) as u8, *out, *outsize);
+    append_data!((cmfflg % 256) as u8, *out, *outsize);
+
+    deflate(options, 2 /* dynamic block */, true /* final */, input, insize, &mut bitpointer, out, outsize);
+
+    append_data!(((checksum >> 24) % 256) as u8, *out, *outsize);
+    append_data!(((checksum >> 16) % 256) as u8, *out, *outsize);
+    append_data!(((checksum >> 8) % 256) as u8, *out, *outsize);
+    append_data!((checksum % 256) as u8, *out, *outsize);
+
+    if (*options).verbose {
+        println_err!("Original Size: {}, Zlib: {}, Compression: {}% Removed", insize, *outsize, 100.0 * (insize - *outsize) as f64 / insize as f64);
+    }
 }
