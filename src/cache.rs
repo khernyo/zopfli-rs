7879c372b2381ae8486a29856cbe4bc0db24e5c4
Szabolcs Berecz <szabolcs.berecz@gmail.com>
2015-11-01 14:56:18 +0100

    Remove unsafe code from cache.rs

--- src/cache.rs
+++ src/cache.rs
@@ -13,71 +13,71 @@ use util::CACHE_LENGTH;
 pub struct LongestMatchCache {
     pub length: Vec<u16>,
     pub dist: Vec<u16>,
-    sublen: Vec<u8>,
+    sublen: Vec<[u8; CACHE_LENGTH * 3]>,
 }
 
 impl LongestMatchCache {
     /// Initializes the ZopfliLongestMatchCache.
     pub fn new(blocksize: usize) -> LongestMatchCache {
-        // Rather large amount of memory.
-        let sublen_size = CACHE_LENGTH * 3 * blocksize;
-
         // length > 0 and dist 0 is invalid combination, which indicates on purpose
         // that this cache value is not filled in yet.
         LongestMatchCache {
             length: iter::repeat(1).take(blocksize).collect(),
             dist: iter::repeat(0).take(blocksize).collect(),
-            sublen: iter::repeat(0).take(sublen_size).collect(),
+            // Rather large amount of memory.
+            sublen: iter::repeat([0; CACHE_LENGTH * 3]).take(blocksize).collect(),
         }
     }
 }
 
 /// Stores sublen array in the cache.
-pub unsafe fn sublen_to_cache(sublen: &[u16; 259],
-                              pos: usize,
-                              length: usize,
-                              lmc: &mut LongestMatchCache) {
+pub fn sublen_to_cache(sublen: &[u16; 259],
+                       pos: usize,
+                       length: usize,
+                       lmc: &mut LongestMatchCache) {
     if CACHE_LENGTH == 0 {
         return;
     }
 
-    let cache: *mut u8 = &mut lmc.sublen[CACHE_LENGTH * pos * 3];
-    if length < 3 {
-        return;
-    }
-
-    let mut j: usize = 0;
     let mut bestlength: u32 = 0;
-    for i in 3..length + 1 {
-        if i == length || sublen[i] != sublen[i + 1] {
-            *cache.offset(j as isize * 3) = (i - 3) as u8;
-            *cache.offset(j as isize * 3 + 1) = (sublen[i] % 256) as u8;
-            *cache.offset(j as isize * 3 + 2) = ((sublen[i] >> 8) % 256) as u8;
-            bestlength = i as u32;
-            j += 1;
-            if j >= CACHE_LENGTH {
-                break;
+    {
+        let cache: &mut [u8; CACHE_LENGTH * 3] = &mut lmc.sublen[pos];
+        if length < 3 {
+            return;
+        }
+
+        let mut j: usize = 0;
+        for i in 3..length + 1 {
+            if i == length || sublen[i] != sublen[i + 1] {
+                cache[j * 3] = (i - 3) as u8;
+                cache[j * 3 + 1] = (sublen[i] % 256) as u8;
+                cache[j * 3 + 2] = ((sublen[i] >> 8) % 256) as u8;
+                bestlength = i as u32;
+                j += 1;
+                if j >= CACHE_LENGTH {
+                    break;
+                }
             }
         }
-    }
 
-    if j < CACHE_LENGTH {
-        assert_eq!(bestlength as usize, length);
-        *cache.offset((CACHE_LENGTH as isize - 1) * 3) = (bestlength - 3) as u8;
-    } else {
-        assert!(bestlength as usize <= length,
-                "{} <= {}",
-                bestlength,
-                length);
+        if j < CACHE_LENGTH {
+            assert_eq!(bestlength as usize, length);
+            cache[(CACHE_LENGTH - 1) * 3] = (bestlength - 3) as u8;
+        } else {
+            assert!(bestlength as usize <= length,
+                    "{} <= {}",
+                    bestlength,
+                    length);
+        }
     }
     assert_eq!(bestlength, max_cached_sublen(lmc, pos, length));
 }
 
 /// Extracts sublen array from the cache.
-pub unsafe fn cache_to_sublen(lmc: *mut LongestMatchCache,
-                              pos: usize,
-                              length: usize,
-                              sublen: &mut [u16; 259]) {
+pub fn cache_to_sublen(lmc: &mut LongestMatchCache,
+                       pos: usize,
+                       length: usize,
+                       sublen: &mut [u16; 259]) {
     if CACHE_LENGTH == 0 {
         return;
     }
@@ -85,13 +85,12 @@ pub unsafe fn cache_to_sublen(lmc: *mut LongestMatchCache,
     if length < 3 {
         return;
     }
-    let cache: *mut u8 = &mut (*lmc).sublen[CACHE_LENGTH * pos * 3];
     let maxlength: u32 = max_cached_sublen(lmc, pos, length);
+    let cache: &mut [u8; CACHE_LENGTH * 3] = &mut (*lmc).sublen[pos];
     let mut prevlength: u32 = 0;
     for j in 0..CACHE_LENGTH {
-        let length: u32 = *cache.offset(j as isize * 3) as u32 + 3;
-        let dist: u32 = *cache.offset(j as isize * 3 + 1) as u32 +
-                        256 * *cache.offset(j as isize * 3 + 2) as u32;
+        let length: u32 = cache[j * 3] as u32 + 3;
+        let dist: u32 = cache[j * 3 + 1] as u32 + 256 * cache[j * 3 + 2] as u32;
         for i in prevlength..length + 1 {
             sublen[i as usize] = dist as u16;
         }
@@ -103,14 +102,14 @@ pub unsafe fn cache_to_sublen(lmc: *mut LongestMatchCache,
 }
 
 /// Returns the length up to which could be stored in the cache.
-pub unsafe fn max_cached_sublen(lmc: *const LongestMatchCache, pos: usize, _length: usize) -> u32 {
+pub fn max_cached_sublen(lmc: &LongestMatchCache, pos: usize, _length: usize) -> u32 {
     if CACHE_LENGTH == 0 {
         return 0;
     }
-    let cache: *const u8 = &(*lmc).sublen[CACHE_LENGTH * pos * 3];
-    if *cache.offset(1) == 0 && *cache.offset(2) == 0 {
+    let cache: &[u8; CACHE_LENGTH * 3] = &lmc.sublen[pos];
+    if cache[1] == 0 && cache[2] == 0 {
         // No sublen cached.
         return 0;
     }
-    *cache.offset(((CACHE_LENGTH - 1) * 3) as isize) as u32 + 3
+    cache[(CACHE_LENGTH - 1) * 3] as u32 + 3
 }
