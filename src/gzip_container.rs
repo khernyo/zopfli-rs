4645141d7470958e8753a44515162e85ebd35b5a
Szabolcs Berecz <szabolcs.berecz@gmail.com>
2015-12-24 00:54:20 +0100

    Remove global crc table to eliminate unsafe code

--- src/gzip_container.rs
+++ src/gzip_container.rs
@@ -6,45 +6,44 @@ use super::Options;
 
 use deflate::deflate;
 
-/// Table of CRCs of all 8-bit messages.
-static mut crc_table: [u64; 256] = [0u64; 256];
+struct CRC {
+    /// Table of CRCs of all 8-bit messages.
+    table: [u64; 256]
+}
 
-/// Flag: has the table been computed? Initially false.
-static mut crc_table_computed: bool = false;
-
-/// Makes the table for a fast CRC.
-unsafe fn make_crc_table() {
-    for n in 0..256 {
-        let mut c: u64 = n as u64;
-        for _ in 0..8 {
-            if c & 1 == 1 {
-                c = 0xedb88320u64 ^ (c >> 1);
-            } else {
-                c = c >> 1;
+impl CRC {
+    /// Makes the table for a fast CRC.
+    fn new() -> CRC {
+        let mut table = [0u64; 256];
+        for n in 0..256 {
+            let mut c: u64 = n as u64;
+            for _ in 0..8 {
+                if c & 1 == 1 {
+                    c = 0xedb88320u64 ^ (c >> 1);
+                } else {
+                    c = c >> 1;
+                }
             }
+            table[n] = c;
         }
-        crc_table[n] = c;
+        CRC { table: table }
     }
-    crc_table_computed = true;
-}
 
-/// Updates a running crc with the bytes buf[0..len-1] and returns
-/// the updated crc. The crc should be initialized to zero.
-unsafe fn update_crc(crc: u64, buf: &[u8]) -> u64 {
-    let mut c: u64 = crc ^ 0xffffffffu64;
+    /// Updates a running crc with the bytes buf[0..len-1] and returns
+    /// the updated crc. The crc should be initialized to zero.
+    fn update(&self, crc: u64, buf: &[u8]) -> u64 {
+        let mut c: u64 = crc ^ 0xffffffffu64;
 
-    if !crc_table_computed {
-        make_crc_table();
+        for v in buf {
+            c = self.table[((c ^ *v as u64) & 0xff) as usize] ^ (c >> 8);
+        }
+        c ^ 0xffffffffu64
     }
-    for v in buf {
-        c = crc_table[((c ^ *v as u64) & 0xff) as usize] ^ (c >> 8);
-    }
-    c ^ 0xffffffffu64
-}
 
-/// Returns the CRC of the bytes buf[0..len-1].
-unsafe fn crc(buf: &[u8]) -> u64 {
-    update_crc(0u64, buf)
+    /// Returns the CRC of the bytes buf[0..len-1].
+    fn calc(&self, buf: &[u8]) -> u64 {
+        self.update(0u64, buf)
+    }
 }
 
 /**
@@ -56,8 +55,9 @@ unsafe fn crc(buf: &[u8]) -> u64 {
  *   be freed after use.
  * outsize: pointer to the dynamic output array size.
  */
-pub unsafe fn compress(options: &Options, input: &[u8]) -> Vec<u8> {
-    let crcvalue: u64 = crc(input);
+pub fn compress(options: &Options, input: &[u8]) -> Vec<u8> {
+    let crc = CRC::new();
+    let crcvalue: u64 = crc.calc(input);
     let mut bp: u8 = 0;
 
     let mut out = Vec::new();
@@ -94,7 +94,7 @@ pub unsafe fn compress(options: &Options, input: &[u8]) -> Vec<u8> {
     out.push(((insize >> 16) % 256) as u8);
     out.push(((insize >> 24) % 256) as u8);
 
-    if (*options).verbose {
+    if options.verbose {
         let outsize = out.len();
         println_err!("Original Size: {}, Gzip: {}, Compression: {}% Removed",
                      insize,
